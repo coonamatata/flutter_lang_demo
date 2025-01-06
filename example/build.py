3b6445c94e582ca6f0711d1ab85b1d4fa9de19d5
EthanThomson <ethan.thomson@exaba.io>
2025-01-07 11:09:49 +1300

    k

--- example/build.py
+++ example/build.py
@@ -3,4 +3,25 @@ import yaml
 with open('assets/i18n/translations.json', 'r', encoding='utf-8') as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
 
-print(data['button']['cancel']['en_US'])
+print(data['button']['cancel'])
+
+#en_GB
+
+#en_US
+
+#vi
+
+#hi
+
+#zh
+
+#ja
+
+#de
+
+#es
+
+#mi
+
+
+
