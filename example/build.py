e8828b13f634869f5956e9075ffa2ecd973a913d
EthanThomson <ethan.thomson@exaba.io>
2025-01-07 11:45:30 +1300

    en_GB done

--- example/build.py
+++ example/build.py
@@ -1,27 +1,49 @@
 import yaml
-
+import json
 with open('assets/i18n/translations.json', 'r', encoding='utf-8') as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
 
 print(data['button']['cancel'])
 
 #en_GB
-en_GBd = {}
-en_GBd['app_bar.title'] = data['app_bar']['title']['en_GB']
-en_GBd['button.cancel'] = data['button']['cancel']['en_GB']
-en_GBd['button.change_language'] = data['button']['change_language']['en_GB']
-en_GBd['language.name'] = data['language']['name']['en_GB']
-en_GBd['language.selected_message'] = data['language']['selected_message']['en_GB']
-en_GBd['language.selection.title'] = data['language']['selection']['title']['en_GB']
-en_GBd['language.selection.message'] = data['language']['selection']['message']['en_GB']
-en_GBd['plural.demo.zero'] = data['plural']['demo']['zero']['en_GB']
-en_GBd['plural.demo.one'] = data['plural']['demo']['one']['en_GB']
-en_GBd['plural.demo.two'] = data['plural']['demo']['two']['en_GB']
-en_GBd['plural.demo.other'] = data['plural']['demo']['other']['en_GB']
-
-
+en_GBd = {
+    "app_bar": {
+        "title": data['app_bar']['title']['en_GB']
+    },
+    "button": {
+        "cancel": data['button']['cancel']['en_GB'],
+        "change_language": data['button']['change_language']['en_GB']
+    },
+    "language": {
+        "name": {
+            "en_US": data['language']['name']['en_US'],
+            "en_GB": data['language']['name']['en_GB'],
+            "zh": data['language']['name']['zh'],
+            "ja": data['language']['name']['ja'],
+            "de": data['language']['name']['de'],
+            "es": data['language']['name']['es'],
+            "hi": data['language']['name']['hi'],
+            "mi": data['language']['name']['mi'],
+            "vi": data['language']['name']['vi']
+        },
+        "selected_message": data['language']['selected_message']['en_GB'],
+        "selection": {
+            "message": data['language']['selection']['message']['en_GB'],
+            "title": data['language']['selection']['title']['en_GB']
+        }
+    },
+    "plural": {
+        "demo": {
+            "zero": data['plural']['demo']['zero']['en_GB'],
+            "one": data['plural']['demo']['one']['en_GB'],
+            "two": data['plural']['demo']['two']['en_GB'],
+            "other": data['plural']['demo']['other']['en_GB']
+        }
+    }
+}
 with open('assets/i18n/en_GB.json', 'w', encoding='utf-8') as f1:
-    en_GBd = yaml.dump(en_GBd, f1, sort_keys=False)
+    json.dump(en_GBd, f1, ensure_ascii=False, indent=4)
+
 #en_US
 
 #vi
